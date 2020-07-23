40fff1fdfb3ad4538992801ec2ef32a128f339ff
harshmalik9423 <harshmalik2013@gmail.com>
2020-07-23 20:42:23 +0530

    Basic add and list added

--- product_inventory.py
+++ product_inventory.py
@@ -5,11 +5,28 @@ class Product:
         self.name = name
         self.price = price
 
+def add_product(name, price):
+    new_product = Product(name,price)
+    products.append(new_product)
+
+def list_products(products):
+    for p in products:
+        print("Product : " + p.name + " and Price : " + str(p.price))
+
+products = []
 while True:
-    print("Add a product")
-    print("Type quit to quit program")
-    command = input("Type a command")
+    print("Type 'add' to add a product")
+    print("Type 'quit' to quit program")
+    print("Type 'list' to list all products")
+    command = input("Type a command: ")
 
     if command == "quit":
         break
+    
+    if command == "add":
+        product_name = input("Enter name of the product : ")
+        product_price = float(input("Enter the price : "))
+        add_product(product_name, product_price)
 
+    elif command == "list":
+        list_products(products)
