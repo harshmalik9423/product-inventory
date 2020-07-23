83096fd8be5a76c531918be53a8870f4b6d540f8
harshmalik9423 <harshmalik2013@gmail.com>
2020-07-23 20:31:23 +0530

    Product class and quit functionality added

--- product_inventory.py
+++ product_inventory.py
@@ -4,3 +4,12 @@ class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
+
+while True:
+    print("Add a product")
+    print("Type quit to quit program")
+    command = input("Type a command")
+
+    if command == "quit":
+        break
+
